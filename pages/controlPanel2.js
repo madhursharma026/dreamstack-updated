54afa2fb8ae8f4ae54d96844a480e652c7854ffe
madhursharma026 <105696243+madhursharma026@users.noreply.github.com>
2023-07-09 23:12:42 +0530

    Uploading Control Panel Page 2

---
+++ pages/controlPanel2.js
@@ -0,0 +1,13 @@
+import Content from "./Components/controlPanel2/Content";
+import Header from "./Components/controlPanel2/Header";
+import Footer from "./Components/controlPanel2/Footer";
+
+export default function controlPanel() {
+  return (
+    <>
+    <Header />
+    <Content />
+    <Footer />
+    </>
+  )
+}
