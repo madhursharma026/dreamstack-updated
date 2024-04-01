0ba82ec115e21cbee63f85ffbdfae8b5f8defa43
madhursharma026 <105696243+madhursharma026@users.noreply.github.com>
2024-04-01 10:26:21 +0530

    Initial Commit

--- pages/Components/WhoWeAre/Section6.js
+++ pages/Components/WhoWeAre/Section6.js
@@ -19,7 +19,7 @@ export default function Section6() {
                                             <h1 className={`${styles.sectionTitle}`}>
                                                 Craftsmanship, as good as it gets
                                             </h1>
-                                            <p className='mt-3' style={{ maxWidth: '400px', fontFamily: 'robotoslablight', lineHeight: '1.6', letterSpacing: '-0.3px', background: '#fff3e4', fontSize: '1.5rem' }}>Building software is best done in small teams, in informal environments. Numbers and metrics-only-driven approaches destroy creativity and result in soulless products.</p>
+                                            <p className={`mt-3 ${styles.h5CustomFont}`} style={{ maxWidth: '400px', fontFamily: 'robotoslablight', lineHeight: '1.6', letterSpacing: '-0.3px', background: '#fff3e4', }}>Building software is best done in small teams, in informal environments. Numbers and metrics-only-driven approaches destroy creativity and result in soulless products.</p>
                                             <p className={`mt-3 ${styles.h5CustomFont}`} style={{ lineHeight: '1.6', letterSpacing: '-0.3px' }}>We see creating great software that delivers high value as a craft, akin to a work of art. You chip away at what's not working, sharpen your skills year after year, and return to refine your product from the very core. While the components might be high-tech, the process used to create them is the same as that of any artisan: tireless effort, countless iteration, and patient refinement.</p>
                                         </div>
                                     </div>
@@ -29,7 +29,7 @@ export default function Section6() {
                                             <h1 className={`${styles.sectionTitle}`}>
                                                 Privately held, profitable, and proud to be
                                             </h1>
-                                            <p className='mt-3' style={{ maxWidth: '400px', fontFamily: 'robotoslablight', lineHeight: '1.6', letterSpacing: '-0.3px', background: '#fff3e4', fontSize: '1.5rem' }}>Our mantra has always been to invest in people and products. That's why most of our investments go towards R&D.</p>
+                                            <p className={`mt-3 ${styles.h5CustomFont}`} style={{ maxWidth: '400px', fontFamily: 'robotoslablight', lineHeight: '1.6', letterSpacing: '-0.3px', background: '#fff3e4', }}>Our mantra has always been to invest in people and products. That's why most of our investments go towards R&D.</p>
                                             <p className={styles.h5CustomFont} style={{ lineHeight: '1.6', letterSpacing: '-0.3px' }}>We've been around for a quarter of a century, and plan to be around a whole lot longer.</p>
                                             <br />
                                             <p className={`mt-3 ${styles.h5CustomFont}`} style={{ lineHeight: '1.6', letterSpacing: '-0.3px' }}>Our decision not to take venture capital, not to go for an IPO, and not to be acquired has given us immense freedom—the freedom to innovate, to learn from our mistakes, and to challenge conventional wisdom. We've put in the time and hard work to build our software portfolio from scratch, rather than acquiring the work of others.</p>
@@ -41,7 +41,7 @@ export default function Section6() {
                                             <h1 className={`${styles.sectionTitle}`}>
                                                 An innovation-first company
                                             </h1>
-                                            <p className='mt-3' style={{ maxWidth: '400px', fontFamily: 'robotoslablight', lineHeight: '1.6', letterSpacing: '-0.3px', background: '#fff3e4', fontSize: '1.5rem' }}>We take great pride in our ability to reinvent ourselves.</p>
+                                            <p className={`mt-3 ${styles.h5CustomFont}`} style={{ maxWidth: '400px', fontFamily: 'robotoslablight', lineHeight: '1.6', letterSpacing: '-0.3px', background: '#fff3e4', }}>We take great pride in our ability to reinvent ourselves.</p>
                                             <p className={styles.h5CustomFont} style={{ lineHeight: '1.6', letterSpacing: '-0.3px' }}>This confidence comes from the wisdom we learned during times when we had no choice but to reinvent, all while tapping into new market opportunities.</p>
                                             <br />
                                             <p className={`mt-3 ${styles.h5CustomFont}`} style={{ lineHeight: '1.6', letterSpacing: '-0.3px' }}>We haven't earned the goodwill of customers and analysts alike by building great software alone, but by demonstrating our commitment to ceaseless innovation.</p>
@@ -53,7 +53,7 @@ export default function Section6() {
                                             <h1 className={`${styles.sectionTitle}`}>
                                                 A holistic approach toward employees
                                             </h1>
-                                            <p className='mt-3' style={{ maxWidth: '400px', fontFamily: 'robotoslablight', lineHeight: '1.6', letterSpacing: '-0.3px', background: '#fff3e4', fontSize: '1.5rem' }}>We hire people for whom our existence truly makes a difference.</p>
+                                            <p className={`mt-3 ${styles.h5CustomFont}`} style={{ maxWidth: '400px', fontFamily: 'robotoslablight', lineHeight: '1.6', letterSpacing: '-0.3px', background: '#fff3e4', }}>We hire people for whom our existence truly makes a difference.</p>
                                             <p className={styles.h5CustomFont} style={{ lineHeight: '1.6', letterSpacing: '-0.3px' }}>We think beyond the system of credentialism. That hiring policy and the goal of creating more jobs in rural communities, by itself, contributes to keeping our culture vital.</p>
                                             <br />
                                             <p className={`mt-3 ${styles.h5CustomFont}`} style={{ lineHeight: '1.6', letterSpacing: '-0.3px' }}>We've had programmers move to creative design, content writers switch to customer support, and copy editors transition into human resource roles. We do not believe that prior experience in one field should preclude pursuing another. Our employees, who’ve stayed with us for more than a decade, have spoken in great detail about such experiences in Humans of Zoho.</p>
@@ -70,7 +70,7 @@ export default function Section6() {
                             <h1 className={`${styles.sectionTitle} mt-4`}>
                                 Craftsmanship, as good as it gets
                             </h1>
-                            <p className='mt-3' style={{ maxWidth: '400px', fontFamily: 'robotoslablight', lineHeight: '1.6', letterSpacing: '-0.3px', background: '#fff3e4', fontSize: '1.5rem' }}>Building software is best done in small teams, in informal environments. Numbers and metrics-only-driven approaches destroy creativity and result in soulless products.</p>
+                            <p className={`mt-3 ${styles.h5CustomFont}`} style={{ maxWidth: '400px', fontFamily: 'robotoslablight', lineHeight: '1.6', letterSpacing: '-0.3px', background: '#fff3e4', }}>Building software is best done in small teams, in informal environments. Numbers and metrics-only-driven approaches destroy creativity and result in soulless products.</p>
                             <p className={`mt-3 ${styles.h5CustomFont}`} style={{ lineHeight: '1.6', letterSpacing: '-0.3px' }}>We see creating great software that delivers high value as a craft, akin to a work of art. You chip away at what's not working, sharpen your skills year after year, and return to refine your product from the very core. While the components might be high-tech, the process used to create them is the same as that of any artisan: tireless effort, countless iteration, and patient refinement.</p>
 
                             <hr className='my-5' />
@@ -78,7 +78,7 @@ export default function Section6() {
                             <h1 className={`${styles.sectionTitle}`}>
                                 Privately held, profitable, and proud to be
                             </h1>
-                            <p className='mt-3' style={{ maxWidth: '400px', fontFamily: 'robotoslablight', lineHeight: '1.6', letterSpacing: '-0.3px', background: '#fff3e4', fontSize: '1.5rem' }}>Our mantra has always been to invest in people and products. That's why most of our investments go towards R&D.</p>
+                            <p className={`mt-3 ${styles.h5CustomFont}`} style={{ maxWidth: '400px', fontFamily: 'robotoslablight', lineHeight: '1.6', letterSpacing: '-0.3px', background: '#fff3e4', }}>Our mantra has always been to invest in people and products. That's why most of our investments go towards R&D.</p>
                             <p className={styles.h5CustomFont} style={{ lineHeight: '1.6', letterSpacing: '-0.3px' }}>We've been around for a quarter of a century, and plan to be around a whole lot longer.</p>
                             <br />
                             <p className={`mt-3 ${styles.h5CustomFont}`} style={{ lineHeight: '1.6', letterSpacing: '-0.3px' }}>Our decision not to take venture capital, not to go for an IPO, and not to be acquired has given us immense freedom—the freedom to innovate, to learn from our mistakes, and to challenge conventional wisdom. We've put in the time and hard work to build our software portfolio from scratch, rather than acquiring the work of others.</p>
@@ -88,7 +88,7 @@ export default function Section6() {
                             <h1 className={`${styles.sectionTitle}`}>
                                 An innovation-first company
                             </h1>
-                            <p className='mt-3' style={{ maxWidth: '400px', fontFamily: 'robotoslablight', lineHeight: '1.6', letterSpacing: '-0.3px', background: '#fff3e4', fontSize: '1.5rem' }}>We take great pride in our ability to reinvent ourselves.</p>
+                            <p className={`mt-3 ${styles.h5CustomFont}`} style={{ maxWidth: '400px', fontFamily: 'robotoslablight', lineHeight: '1.6', letterSpacing: '-0.3px', background: '#fff3e4', }}>We take great pride in our ability to reinvent ourselves.</p>
                             <p className={styles.h5CustomFont} style={{ lineHeight: '1.6', letterSpacing: '-0.3px' }}>This confidence comes from the wisdom we learned during times when we had no choice but to reinvent, all while tapping into new market opportunities.</p>
                             <br />
                             <p className={`mt-3 ${styles.h5CustomFont}`} style={{ lineHeight: '1.6', letterSpacing: '-0.3px' }}>We haven't earned the goodwill of customers and analysts alike by building great software alone, but by demonstrating our commitment to ceaseless innovation.</p>
@@ -98,10 +98,10 @@ export default function Section6() {
                             <h1 className={`${styles.sectionTitle}`}>
                                 A holistic approach toward employees
                             </h1>
-                            <p className='mt-3' style={{ maxWidth: '400px', fontFamily: 'robotoslablight', lineHeight: '1.6', letterSpacing: '-0.3px', background: '#fff3e4', fontSize: '1.5rem' }}>We hire people for whom our existence truly makes a difference.</p>
-                            <h5 className={`mt-3 ${styles.h5CustomFont}`} style={{ lineHeight: '1.6', letterSpacing: '-0.3px' }}>We think beyond the system of credentialism. That hiring policy and the goal of creating more jobs in rural communities, by itself, contributes to keeping our culture vital.</h5>
+                            <p className={`mt-3 ${styles.h5CustomFont}`} style={{ maxWidth: '400px', fontFamily: 'robotoslablight', lineHeight: '1.6', letterSpacing: '-0.3px', background: '#fff3e4', }}>We hire people for whom our existence truly makes a difference.</p>
+                            <p className={`mt-3 ${styles.h5CustomFont}`} style={{ lineHeight: '1.6', letterSpacing: '-0.3px' }}>We think beyond the system of credentialism. That hiring policy and the goal of creating more jobs in rural communities, by itself, contributes to keeping our culture vital.</p>
                             <br />
-                            <h5 className={`mt-3 ${styles.h5CustomFont}`} style={{ lineHeight: '1.6', letterSpacing: '-0.3px' }}>We've had programmers move to creative design, content writers switch to customer support, and copy editors transition into human resource roles. We do not believe that prior experience in one field should preclude pursuing another. Our employees, who’ve stayed with us for more than a decade, have spoken in great detail about such experiences in Humans of Zoho.</h5>
+                            <p className={`mt-3 ${styles.h5CustomFont}`} style={{ lineHeight: '1.6', letterSpacing: '-0.3px' }}>We've had programmers move to creative design, content writers switch to customer support, and copy editors transition into human resource roles. We do not believe that prior experience in one field should preclude pursuing another. Our employees, who’ve stayed with us for more than a decade, have spoken in great detail about such experiences in Humans of Zoho.</p>
                         </div>
                     </div>
                 </div>
--- pages/Components/WhoWeAre/Section7.js
+++ pages/Components/WhoWeAre/Section7.js
@@ -8,36 +8,36 @@ export default function Section7() {
                     <h1 className={`${styles.sectionTitle} pt-md-5 pt-3`}>
                         Our journey through the years
                     </h1>
-                    <p className={`mt-3 ${styles.h5CustomFont}`} style={{ maxWidth: '800px', margin: 'auto', lineHeight: '1.6', letterSpacing: '-0.3px' }}>
+                    <p className={`mt-3 ${styles.customFont}`} style={{ maxWidth: '800px', margin: 'auto', lineHeight: '1.6', letterSpacing: '-0.3px' }}>
                         Creating stellar products for our users comes first. In the long run, it's not the big wins that matter, but the little ones, and the determination to keep trying. And that's what we do every day: continually put new things on the market and demonstrate the value of our products.
                     </p>
                 </div>
-                <img src="https://www.zohowebstatic.com/sites/zweb/images/zoho_general_pages/zh-ourstory-bg.png" alt="#ImgNotFound" className='w-100 mt-lg-0 mt-3 d-lg-block d-none' style={{marginBottom: '-250px'}} />
+                <img src="https://www.zohowebstatic.com/sites/zweb/images/zoho_general_pages/zh-ourstory-bg.png" alt="#ImgNotFound" className='w-100 mt-lg-0 mt-3 d-lg-block d-none' style={{ marginBottom: '-250px' }} />
 
-                <div className="row p-5 text-white mt-5" style={{maxWidth:'700px', margin: 'auto', borderRadius: '10px', background: '#0060B4', zIndex: '1'}}>
+                <div className="row p-5 text-white mt-5" style={{ maxWidth: '700px', margin: 'auto', borderRadius: '10px', background: '#0060B4', zIndex: '1' }}>
                     <div className="col-md-4 col-6 mt-3">
                         <h1>25+</h1>
-                        <h6 style={{fontSize: '14px'}}>YEARS IN BUSINESS</h6>
+                        <p className={styles.customFont}>YEARS IN BUSINESS</p>
                     </div>
                     <div className="col-md-4 col-6 mt-3">
                         <h1>15K+</h1>
-                        <h6 style={{fontSize: '14px'}}>EMPLOYEES</h6>
+                        <p className={styles.customFont}>EMPLOYEES</p>
                     </div>
                     <div className="col-md-4 col-6 mt-3">
                         <h1>150+</h1>
-                        <h6 style={{fontSize: '14px'}}>COUNTRIES SERVED</h6>
+                        <p className={styles.customFont}>COUNTRIES SERVED</p>
                     </div>
                     <div className="col-md-4 col-6 mt-3">
                         <h1>16</h1>
-                        <h6 style={{fontSize: '14px'}}>DATA CENTERS</h6>
+                        <p className={styles.customFont}>DATA CENTERS</p>
                     </div>
                     <div className="col-md-4 col-6 mt-3">
                         <h1>100M+</h1>
-                        <h6 style={{fontSize: '14px'}}>USERS</h6>
+                        <p className={styles.customFont}>USERS</p>
                     </div>
                     <div className="col-md-4 col-6 mt-3">
                         <h1>55+</h1>
-                        <h6 style={{fontSize: '14px'}}>PRODUCTS</h6>
+                        <p className={styles.customFont}>PRODUCTS</p>
                     </div>
                 </div>
             </div>
