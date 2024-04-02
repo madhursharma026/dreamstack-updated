486279ce8c04fc9f4c01d59c442d62c3267ab92c
madhursharma026 <105696243+madhursharma026@users.noreply.github.com>
2024-04-02 17:27:32 +0530

    Initial Commit

--- pages/Components/Blockchain/BlockchainServices.js
+++ pages/Components/Blockchain/BlockchainServices.js
@@ -1,5 +1,6 @@
 import Images16 from "../../media/Blockchain-Images/blockchain-image-16.png";
 import styles from '../../../styles/Blockchain/BlockchainServices.module.css';
+import Link from "next/link";
 
 export default function BlockchainServices() {
     return (
@@ -16,56 +17,56 @@ export default function BlockchainServices() {
 
                         <div className="row px-xxl-5">
                             <div className="col-lg-6 row mt-4">
-                                <div className="col-2 col-sm-1 col-lg-2 col-xl-1"><img src={Images16.src} alt="#ImgNotFound" style={{width: '100%'}} /></div>
+                                <div className="col-2 col-sm-1 col-lg-2 col-xl-1"><img src={Images16.src} alt="#ImgNotFound" style={{ width: '100%' }} /></div>
                                 <div className="col-10 col-sm-11 col-lg-10 col-xl-11">
                                     <p className={styles.h3CustomFont}>Blockchain Application Development</p>
                                     <p className={`${styles.h5CustomFont}`}>Our public and private blockchain app development helps you build distributed applications and SaaS products while giving full control over users to you. We will give you a picture of how blockchain can be a perfect fit for your business model</p>
                                 </div>
                             </div>
                             <div className="col-lg-6 row mt-4">
-                                <div className="col-2 col-sm-1 col-lg-2 col-xl-1"><img src={Images16.src} alt="#ImgNotFound" style={{width: '100%'}} /></div>
+                                <div className="col-2 col-sm-1 col-lg-2 col-xl-1"><img src={Images16.src} alt="#ImgNotFound" style={{ width: '100%' }} /></div>
                                 <div className="col-10 col-sm-11 col-lg-10 col-xl-11">
                                     <p className={styles.h3CustomFont}>dApps Development</p>
                                     <p className={`${styles.h5CustomFont}`}>Enhance customer data security, save operating expenses, and get rid of any centralized failure spots by entering into the new world of P2P decentralized apps (dApps) development. Implement user-oriented products on public blockchain networks</p>
                                 </div>
                             </div>
                             <div className="col-lg-6 row mt-4">
-                                <div className="col-2 col-sm-1 col-lg-2 col-xl-1"><img src={Images16.src} alt="#ImgNotFound" style={{width: '100%'}} /></div>
+                                <div className="col-2 col-sm-1 col-lg-2 col-xl-1"><img src={Images16.src} alt="#ImgNotFound" style={{ width: '100%' }} /></div>
                                 <div className="col-10 col-sm-11 col-lg-10 col-xl-11">
                                     <p className={styles.h3CustomFont}>Blockchain Wallet Development</p>
                                     <p className={`${styles.h5CustomFont}`}>Build a highly safe, secure, and standalone blockchain wallet for cryptocurrencies with hot and cold storage features, allowing you to transfer various digital currencies and assets. We will also integrate a multi-coin blockchain wallet into your existing system</p>
                                 </div>
                             </div>
                             <div className="col-lg-6 row mt-4">
-                                <div className="col-2 col-sm-1 col-lg-2 col-xl-1"><img src={Images16.src} alt="#ImgNotFound" style={{width: '100%'}} /></div>
+                                <div className="col-2 col-sm-1 col-lg-2 col-xl-1"><img src={Images16.src} alt="#ImgNotFound" style={{ width: '100%' }} /></div>
                                 <div className="col-10 col-sm-11 col-lg-10 col-xl-11">
                                     <p className={styles.h3CustomFont}>NFT Marketplace Development</p>
                                     <p className={`${styles.h5CustomFont}`}>Our enterprise blockchain solutions also include developing protocol-specific decentralized NFT marketplaces to organize NFT marketplaces trading, bidding, and selling digital assets. Our cross-chain marketplaces are also compatible with your multichain NFTs</p>
                                 </div>
                             </div>
                             <div className="col-lg-6 row mt-4">
-                                <div className="col-2 col-sm-1 col-lg-2 col-xl-1"><img src={Images16.src} alt="#ImgNotFound" style={{width: '100%'}} /></div>
+                                <div className="col-2 col-sm-1 col-lg-2 col-xl-1"><img src={Images16.src} alt="#ImgNotFound" style={{ width: '100%' }} /></div>
                                 <div className="col-10 col-sm-11 col-lg-10 col-xl-11">
                                     <p className={styles.h3CustomFont}>Smart Contracts Development</p>
                                     <p className={`${styles.h5CustomFont}`}>Increase the speed, effectiveness, and security of the process of your transactions with blockchain smart contracts. It will surely reduce paperwork, digitize data transfers without the need for third-party, and improve security</p>
                                 </div>
                             </div>
                             <div className="col-lg-6 row mt-4">
-                                <div className="col-2 col-sm-1 col-lg-2 col-xl-1"><img src={Images16.src} alt="#ImgNotFound" style={{width: '100%'}} /></div>
+                                <div className="col-2 col-sm-1 col-lg-2 col-xl-1"><img src={Images16.src} alt="#ImgNotFound" style={{ width: '100%' }} /></div>
                                 <div className="col-10 col-sm-11 col-lg-10 col-xl-11">
                                     <p className={styles.h3CustomFont}>DeFi Development</p>
                                     <p className={`${styles.h5CustomFont}`}>Our Decentralized Finance (DeFi) development services make the most of decentralized networks to transform financial applications into permissionless, transparent, self-executing protocols that don't need any middlemen to be governed</p>
                                 </div>
                             </div>
                             <div className="col-lg-6 row mt-4">
-                                <div className="col-2 col-sm-1 col-lg-2 col-xl-1"><img src={Images16.src} alt="#ImgNotFound" style={{width: '100%'}} /></div>
+                                <div className="col-2 col-sm-1 col-lg-2 col-xl-1"><img src={Images16.src} alt="#ImgNotFound" style={{ width: '100%' }} /></div>
                                 <div className="col-10 col-sm-11 col-lg-10 col-xl-11">
                                     <p className={styles.h3CustomFont}>ICO Development</p>
                                     <p className={`${styles.h5CustomFont}`}>Our extensive knowledge of blockchain can help you build a robust, scalable, and fully compliant Initial Coin Offering (ICO) Management platform to allow investors to track KPIs and manage the community, reports, and crowdfunding</p>
                                 </div>
                             </div>
                             <div className="col-lg-6 row mt-4">
-                                <div className="col-2 col-sm-1 col-lg-2 col-xl-1"><img src={Images16.src} alt="#ImgNotFound" style={{width: '100%'}} /></div>
+                                <div className="col-2 col-sm-1 col-lg-2 col-xl-1"><img src={Images16.src} alt="#ImgNotFound" style={{ width: '100%' }} /></div>
                                 <div className="col-10 col-sm-11 col-lg-10 col-xl-11">
                                     <p className={styles.h3CustomFont}>STO Development</p>
                                     <p className={`${styles.h5CustomFont}`}>As a prominent Security Token Offering (STO) service provider, we have helped many investors in making safe investments while adhering to the regulations and policies. Our experience can take your business to the next level of crypto-based investing</p>
@@ -85,7 +86,7 @@ export default function BlockchainServices() {
                         </p>
                     </div>
                     <div className="col-lg-2 mt-3 text-center">
-                        <button type="button" className="btn btn-primary px-3 py-2"><b>Talk to us</b></button>
+                        <Link className={`btn btn-primary px-3 py-2`} href="/contact" role="button"><b>Talk to us</b></Link>
                     </div>
                 </div>
             </div>
--- pages/Components/Blockchain/WhyChoose.js
+++ pages/Components/Blockchain/WhyChoose.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import styles from '../../../styles/Blockchain/WhyChoose.module.css';
 import Images15 from "../../media/Blockchain-Images/blockchain-image-15.jpg";
 
@@ -30,7 +31,7 @@ export default function WhyChoose() {
                                 </div>
                             </div>
                             <div className={`col-xl-4 ${styles.imgStyle}`}>
-                            <img src={Images15.src} alt="#ImgNotFound" width='100%' />
+                                <img src={Images15.src} alt="#ImgNotFound" width='100%' />
                             </div>
                             <div className="col-md-6 col-xl-4">
                                 <div className="card w-100" style={{ border: '0', borderRadius: '0', background: 'transparent' }}>
@@ -52,14 +53,15 @@ export default function WhyChoose() {
                 <div className='py-5' style={{ background: '#31BBAE', color: 'white' }}>
                     <div className={`px-3 px-md-5 py-5 ${styles.ContainerWidth} row`}>
                         <div className="col-lg-10">
-                        <p className={styles.h2CustomFont}>
+                            <p className={styles.h2CustomFont}>
                                 Want to Develop Fast, Secure, and Scalable Blockchain
                                 <br />
                                 Application with Our Expertise?
                             </p>
                         </div>
                         <div className="col-lg-2 mt-3 text-center">
-                            <button type="button" className="btn btn-primary px-3 py-2"><b>Talk to us</b></button>
+                            {/* <Link href="/contact" role="button" className="btn btn-primary px-3 py-2"><b>Talk to us</b></Link> */}
+                            <Link className={`btn btn-primary px-3 py-2`} href="/contact" role="button"><b>Talk to us</b></Link>
                         </div>
                     </div>
                 </div>
