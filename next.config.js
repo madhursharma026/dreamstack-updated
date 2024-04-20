b5ab5ff097c61d6347b03cf88285c8f225d10f3c
madhursharma026 <105696243+madhursharma026@users.noreply.github.com>
2024-04-20 12:06:38 +0530

    Initial Commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+// next.config.js
+
+module.exports = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mp4|webm)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next',
+          name: 'static/media/[name].[hash].[ext]',
+        },
+      },
+    });
+
+    return config;
+  },
+};
